28b7123e48734c5a379fc5c3a9b536bf951000f0
Vy NN <vypois@gmail.com>
2020-11-15 16:38:13 +0700

    Topic 13 Handle Textbox/TextArea

--- selenium/webdriver/Topic_01_Check_Environment.java
+++ selenium/webdriver/Topic_01_Check_Environment.java
@@ -1,5 +1,6 @@
 package webdriver;
 
+import java.util.Random;
 import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.By;
@@ -12,43 +13,161 @@ import org.testng.annotations.Test;
 
 public class Topic_01_Check_Environment {
 	WebDriver driver;
+	String email, userID, password, loginPageURL, customer_ID;
+	String name, dobInput, dobOutput, address, city, state, pin, phone;
+	String editAddress, editCity, editState, editPin, editPhone, editEmail;
+
+	By nameBy = By.name("name");
+	By dobBy = By.name("dob");
+	By addressBy = By.name("addr");
+	By cityBy = By.name("city");
+	By stateBy = By.name("state");
+	By pinBy = By.name("pinno");
+	By phoneBy = By.name("telephoneno");
+	By emailBy = By.name("emailid");
+	By passwordBy = By.name("password");
 
 	@BeforeClass
 	public void beforeClass() {
 		driver = new FirefoxDriver();
-		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+		driver.manage().timeouts().implicitlyWait(35, TimeUnit.SECONDS);
 		driver.manage().window().maximize();
 		driver.get("http://demo.guru99.com/v4/");
+
+		name = "Donald Trump";
+		dobInput = "04/05/1958";
+		dobOutput = "1958-04-05";
+		address = "072 Kensington Road";
+		city = "Clearwater";
+		state = "Florida";
+		pin = "453564";
+		phone = "7272490462";
+		email = generateEmail();
+
+		loginPageURL = driver.getCurrentUrl();
+
+		editAddress = "2537 Saint Paul Hill";
+		editCity = "Katy";
+		editState = "Texas";
+		editPin = "345235";
+		editPhone = "8321825775";
+		editEmail = generateEmail();
 	}
 
 	@Test
-	public void TC_01_ValidateCurrentUrl() {
-		// Login Page Url matching
-		String loginPageUrl = driver.getCurrentUrl();
-		Assert.assertEquals(loginPageUrl, "http://demo.guru99.com/v4/");
+	public void TC_01_Register() {
+		driver.findElement(By.xpath("//a[text()='here']")).click();
+		driver.findElement(By.name("emailid")).sendKeys(email);
+		driver.findElement(By.name("btnLogin")).click();
+
+		userID = driver.findElement(By.xpath("//td[text()='User ID :']/following-sibling::td")).getText();
+		password = driver.findElement(By.xpath("//td[text()='Password :']/following-sibling::td")).getText();
+
 	}
 
 	@Test
-	public void TC_02_ValidatePageTitle() {
-		// Login Page title
-		String loginPageTitle = driver.getTitle();
-		Assert.assertEquals(loginPageTitle, "Guru99 Bank Home Page");
+	public void TC_02_Login() {
+		driver.get(loginPageURL);
+		driver.findElement(By.name("uid")).sendKeys(userID);
+		driver.findElement(By.name("password")).sendKeys(password);
+		driver.findElement(By.name("btnLogin")).click();
+
+		Assert.assertEquals(driver.findElement(By.xpath("//marquee[@class='heading3']")).getText(),
+				"Welcome To Manager's Page of Guru99 Bank");
+
 	}
 
 	@Test
-	public void TC_03_LoginFormDisplayed() {
-		// Login form displayed
-		Assert.assertTrue(driver.findElement(By.xpath("//form[@name='frmLogin']")).isDisplayed());
+	public void TC_03_New_Customer() throws InterruptedException {
+		driver.findElement(By.xpath("//a[text()='New Customer']")).click();
+
+		driver.findElement(nameBy).sendKeys(name);
+		driver.findElement(dobBy).sendKeys(dobInput);
+		driver.findElement(addressBy).sendKeys(address);
+		driver.findElement(cityBy).sendKeys(city);
+		driver.findElement(stateBy).sendKeys(state);
+		driver.findElement(pinBy).sendKeys(pin);
+		driver.findElement(phoneBy).sendKeys(phone);
+		driver.findElement(emailBy).sendKeys(email);
+		driver.findElement(passwordBy).sendKeys(password);
+		
+		driver.findElement(By.name("sub")).click();
+
+		// Verify data
+		Assert.assertEquals(driver.findElement(By.xpath("//p[@class='heading3']")).getText(),
+				"Customer Registered Successfully!!!");
+
+		Assert.assertEquals(
+				driver.findElement(By.xpath("//td[text()='Customer Name']/following-sibling::td")).getText(), name);
+		Assert.assertEquals(driver.findElement(By.xpath("//td[text()='Birthdate']/following-sibling::td")).getText(),
+				dobOutput);
+		Assert.assertEquals(driver.findElement(By.xpath("//td[text()='Address']/following-sibling::td")).getText(),
+				address);
+		Assert.assertEquals(driver.findElement(By.xpath("//td[text()='City']/following-sibling::td")).getText(), city);
+		Assert.assertEquals(driver.findElement(By.xpath("//td[text()='State']/following-sibling::td")).getText(),
+				state);
+		Assert.assertEquals(driver.findElement(By.xpath("//td[text()='Pin']/following-sibling::td")).getText(), pin);
+		Assert.assertEquals(driver.findElement(By.xpath("//td[text()='Mobile No.']/following-sibling::td")).getText(),
+				phone);
+		Assert.assertEquals(driver.findElement(By.xpath("//td[text()='Email']/following-sibling::td")).getText(),
+				email);
+
+		customer_ID = driver.findElement(By.xpath("//td[text()='Customer ID']/following-sibling::td")).getText();
 	}
-	
+
 	@Test
-	public void TC_04_LoginFormDisplayed() {
-		// Login form displayed
-		Assert.assertTrue(driver.findElement(By.xpath("//form[@name='frmLogin']")).isDisplayed());
+	public void TC_04_Edit_Customer() {
+		driver.findElement(By.xpath("//a[text()='Edit Customer']")).click();
+		driver.findElement(By.name("cusid")).sendKeys(customer_ID);
+		driver.findElement(By.name("AccSubmit")).click();
+		
+		// Verify value at Edit customer page
+		Assert.assertEquals(driver.findElement(nameBy).getAttribute("value"), name);
+		Assert.assertEquals(driver.findElement(dobBy).getAttribute("value"), dobOutput);
+		Assert.assertEquals(driver.findElement(addressBy).getAttribute("value"), address);
+		Assert.assertEquals(driver.findElement(cityBy).getAttribute("value"), city);
+		Assert.assertEquals(driver.findElement(stateBy).getAttribute("value"), state);
+		Assert.assertEquals(driver.findElement(pinBy).getAttribute("value"), pin);
+		Assert.assertEquals(driver.findElement(phoneBy).getAttribute("value"), phone);
+		Assert.assertEquals(driver.findElement(emailBy).getAttribute("value"), email);
+		
+		//Edit customer
+		driver.findElement(addressBy).clear();
+		driver.findElement(addressBy).sendKeys(editAddress);
+		driver.findElement(cityBy).clear();
+		driver.findElement(cityBy).sendKeys(editCity);
+		driver.findElement(stateBy).clear();
+		driver.findElement(stateBy).sendKeys(editState);
+		driver.findElement(pinBy).clear();
+		driver.findElement(pinBy).sendKeys(editPin);
+		driver.findElement(phoneBy).clear();
+		driver.findElement(phoneBy).sendKeys(editPhone);
+		driver.findElement(emailBy).clear();
+		driver.findElement(emailBy).sendKeys(editEmail);
+		driver.findElement(By.name("sub")).click();
+		
+		Assert.assertEquals(driver.findElement(By.xpath("//p[@class='heading3']")).getText(), "Customer details updated Successfully!!!");
+		
+		Assert.assertEquals(driver.findElement(By.xpath("//td[text()='Address']/following-sibling::td")).getText(),
+				editAddress);
+		Assert.assertEquals(driver.findElement(By.xpath("//td[text()='City']/following-sibling::td")).getText(), editCity);
+		Assert.assertEquals(driver.findElement(By.xpath("//td[text()='State']/following-sibling::td")).getText(),
+				editState);
+		Assert.assertEquals(driver.findElement(By.xpath("//td[text()='Pin']/following-sibling::td")).getText(), editPin);
+		Assert.assertEquals(driver.findElement(By.xpath("//td[text()='Mobile No.']/following-sibling::td")).getText(),
+				editPhone);
+		Assert.assertEquals(driver.findElement(By.xpath("//td[text()='Email']/following-sibling::td")).getText(),
+				editEmail);
+	}
+
+	public String generateEmail() {
+		Random rand = new Random();
+		return "donald" + rand.nextInt(99999) + "@github.io";
 	}
 
 	@AfterClass
 	public void afterClass() {
 		driver.quit();
 	}
+
 }
